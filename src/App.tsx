3fbb87938760049a37d30c4b09b29d22db02f60c
Chen Xiongjie <chen.xcmail@gmail.com>
2022-10-18 15:48:32 +0200

    updated: default data

--- src/App.tsx
+++ src/App.tsx
@@ -11,42 +11,22 @@ import { grey } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
 function App(props:{html?:string}) { 
-   
-  let sampleData:Array<BlockInfo> = [
-    {
-    type:'heading',
-    content: {
-      layout: {},
-      data: {
-        text: "UN whistleblowing complaints 'dismissed too early'",
-        style: {level: 1}
-      }
-    },
-  },
-  {
-    type:'full_image',
-    content: {
-      layout: {},
-      data: {
-        src: "https://ichef.bbci.co.uk/news/976/cpsprodpb/83F8/production/_125548733_stillspurna3new.jpg",
-        style: {padding: 0, borderWidth:0, background:'#ffffff'}
-      }
-    },
-  },
-  {
-    type:'p',
-    content: {
-      layout: {padding: 10},
-      data:  "<p>Purna Sen's <a href=\"https://www.bbc.com\">comments</a> follow a BBC investigation which revealed the sackings of a number of UN staff who tried to expose alleged wrongdoing. </p><p>Ms Sen said the UN should \"step up\" and adopt any suggestions made by a panel. </p>  The UN has said it is committed to protecting \"bona fide whistleblowers\" and holding staff accountable.    </p><p>   The BBC documentary, The Whistleblowers: Inside the UN, features accounts from staff members who tried to report allegations - including fraud and sexual abuse. All said they had been penalised after speaking out - and some were sacked.</p>"      
-    }
-  }
-  ];
+  let data = [{
+    type:'imagetext',
+    settings:{childrenHorizontal: true},
+    children: [
+    {type:'text', content:'<p>Hello</p><p>Good</p>', settings:{}},
+    {type:'image', content:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOdlzDI3LftAb4bwkJOWODiyLE9bpB3Wr8r9A60RGy1A&s', settings:{}}
+    ], allowedType:["text"]},
+    {type:'text', content:'<p>Content</p>', settings:{}},
+];
 
   return (
     <div className="App">
       <header className="App-header">
       </header>
-        <DMEditor data={props.html?[{type:'p', content:{layout:{}, data:props.html}}]:sampleData} /></div>
+        <DMEditor data={props.html?[{type:'p', content:{layout:{}, data:props.html}}]:data} />        
+        </div>
   );
 }
 
