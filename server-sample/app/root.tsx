691789e95d2772fb5a78f0b86f95c17b6c1b3bce
Chen Xiongjie <chen.xcmail@gmail.com>
2022-11-12 01:01:34 +0100

    added: server-sample to render dmeditor to html

---
+++ server-sample/app/root.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { Component } from 'react'
+
+export default class Root extends Component<{},{}> {
+  render() {
+    return(
+      <div>
+        <p> ROOT, NOTHING HERE... THIS IS JUST A PLACEHOLDER</p>
+      </div>
+    )
+  }
+} 
---
+++ server-sample/webpack.config.js
@@ -0,0 +1,60 @@
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  context: path.join(__dirname, '/src'),
+  entry: {
+    app: ['es6-promise', 'whatwg-fetch', './app.js'],
+  },
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: 'app.js',
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!',
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]',
+      },
+    ],
+  },
+
+  devtool: null,
+
+//   plugins: NODE_ENV === 'development' ? [
+//     new webpack.NoErrorsPlugin(),
+//   ] : [
+//     new webpack.NoErrorsPlugin(),
+//     new webpack.optimize.UglifyJsPlugin({
+//       compress: { warnings: false },
+//     }),
+//     new ExtractTextPlugin('css/[name][hash].css'),
+//     new HtmlWebpackPlugin({
+//       title: 'ReactJS Routing',
+//       filename: '../index.html',
+//       hash: true,
+//     }),
+//     new CleanWebpackPlugin(['build'], {
+//       root: __dirname,
+//       verbose: true,
+//       dry: false,
+//     }),
+//   ],
+  devServer: {},
+};
